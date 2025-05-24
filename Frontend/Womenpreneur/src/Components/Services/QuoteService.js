9bca3d0a832a721f7f35df5832c1a0b243be4e68
rathod1234567 <rathodravinder181@gmail.com>
2025-05-24 12:58:36 +0530

    backend url added

--- Frontend/Womenpreneur/src/Components/Services/QuoteService.js
+++ Frontend/Womenpreneur/src/Components/Services/QuoteService.js
@@ -1,6 +1,6 @@
 export const fetchQuote = async () => {
   try {
-    const response = await fetch('http://localhost:5000/api/quote');
+    const response = await fetch('https://womenpreneur-task.onrender.com/api/quote');
 console.log(response)
     if (!response.ok) {
       throw new Error('Failed to fetch quote');
